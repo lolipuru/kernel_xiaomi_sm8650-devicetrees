2b9f91e279bd597cb6a3c2a0466943b70838c8af
Bruno Martins <bgcngm@gmail.com>
2024-06-26 21:31:36 +0200

    treewide: Opt-out for irrelevant DTB[O]s based on config
    Change-Id: Ic2bcdb5533f21005708868e4168391563c9961c7

--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -1,16 +1,26 @@
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	:= pineapple-camera.dtbo
+
+ifeq ($(CONFIG_TARGET_PRODUCT_HOUJI),y)
+dtbo-y += houji-sm8650-camera-sensor.dtbo
+else ifeq ($(CONFIG_TARGET_PRODUCT_SHENNONG),y)
+dtbo-y += shennong-sm8650-camera-sensor.dtbo
+else ifeq ($(CONFIG_TARGET_PRODUCT_SUIREN),y)
+dtbo-y += suiren-sm8650-camera-sensor.dtbo
+else ifeq ($(CONFIG_TARGET_PRODUCT_MANET),y)
+dtbo-y += manet-sm8650-camera-sensor.dtbo
+else ifeq ($(CONFIG_TARGET_PRODUCT_AURORA),y)
+dtbo-y += aurora-sm8650-camera-sensor.dtbo
+else ifeq ($(CONFIG_TARGET_PRODUCT_RUYI),y)
+dtbo-y += ruyi-sm8650-camera-sensor.dtbo
+else ifeq ($(CONFIG_TARGET_PRODUCT_GOKU),y)
+dtbo-y += goku-sm8650-camera-sensor.dtbo
+else
 dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-cdp.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
 									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
-									shennong-sm8650-camera-sensor.dtbo \
-									manet-sm8650-camera-sensor.dtbo \
-									houji-sm8650-camera-sensor.dtbo  \
-									suiren-sm8650-camera-sensor.dtbo  \
-									aurora-sm8650-camera-sensor.dtbo \
-									ruyi-sm8650-camera-sensor.dtbo \
-									goku-sm8650-camera-sensor.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
 									pineapple-camera-sensor-aim500.dtbo
+endif
 
