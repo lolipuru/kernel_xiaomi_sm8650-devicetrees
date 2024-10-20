f2f144579649643b3bb4b9b262fb86f2420ebd0f
lolipuru <banstellazz@gmail.com>
2024-10-20 22:32:54 +0700

    dts: camera: import xiaomi camera changes
    from goku-u-oss-test
    
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -4,8 +4,19 @@ dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-mtp.dtbo \
 									pineapple-camera-sensor-hdk.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
+									shennong-sm8650-camera-sensor.dtbo \
+									manet-sm8650-camera-sensor.dtbo \
+									houji-sm8650-camera-sensor.dtbo  \
+									suiren-sm8650-camera-sensor.dtbo  \
+									aurora-sm8650-camera-sensor.dtbo \
+									ruyi-sm8650-camera-sensor.dtbo \
+									goku-sm8650-camera-sensor.dtbo \
+									pineapple-camera-sensor-qrd.dtbo \
 									pineapple-camera-sensor-aim500.dtbo
+
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
 dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
 								cliffs-camera-sensor-mtp.dtbo \
-								cliffs-camera-sensor-qrd.dtbo
+								cliffs-camera-sensor-qrd.dtbo \
+								peridot-sm8635-camera-sensor.dtbo\
+								chenfeng-sm8635-camera-sensor.dtbo
