5df05364c93b5e75b52324d7e38124527c3d8406
lolipuru <banstellazz@gmail.com>
2024-10-27 16:26:54 +0700

    treewide: separated cliffs from pineapple
    Signed-off-by: lolipuru <banstellazz@gmail.com>

---
+++ qcom/camera/config/cliffs.mk
@@ -0,0 +1,6 @@
+dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
+dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
+								cliffs-camera-sensor-mtp.dtbo \
+								cliffs-camera-sensor-qrd.dtbo \
+								peridot-sm8635-camera-sensor.dtbo\
+								chenfeng-sm8635-camera-sensor.dtbo
--- qcom/camera/config/pineapple.mk
+++ qcom/camera/config/pineapple.mk
@@ -14,9 +14,3 @@ dtbo-$(CONFIG_ARCH_PINEAPPLE)	+= pineapple-camera-v2.dtbo \
 									pineapple-camera-sensor-qrd.dtbo \
 									pineapple-camera-sensor-aim500.dtbo
 
-dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera.dtbo
-dtbo-$(CONFIG_ARCH_CLIFFS)	+= cliffs-camera-sensor-cdp.dtbo \
-								cliffs-camera-sensor-mtp.dtbo \
-								cliffs-camera-sensor-qrd.dtbo \
-								peridot-sm8635-camera-sensor.dtbo\
-								chenfeng-sm8635-camera-sensor.dtbo
